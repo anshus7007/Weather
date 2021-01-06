6b72c71d049ccec46de03a104778ee2dd38bda44
anshus7007 <60043003+anshus7007@users.noreply.github.com>
2021-01-07 00:20:50 +0530

    added bnv and fab

--- app/src/main/java/com/anshu/weather/MainActivity.kt
+++ app/src/main/java/com/anshu/weather/MainActivity.kt
@@ -1,17 +1,32 @@
 package com.anshu.weather
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var fab:FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        fab=findViewById(R.id.fab)
+        fab.setOnClickListener{
+            val i = Intent(this, HomeActivity::class.java)
+            overridePendingTransition(
+                android.R.anim.accelerate_decelerate_interpolator,
+                android.R.anim.accelerate_decelerate_interpolator
+            )
+            startActivity(i)
+        }
+        bottomNavigationView.background=null
+        bottomNavigationView.menu.getItem(1).isEnabled=false
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener)
-        supportFragmentManager.beginTransaction().replace(R.id.flFragment, HomeFragment())
+        supportFragmentManager.beginTransaction().replace(R.id.flFragment, SearchFragment())
             .commit()
 
     }
@@ -20,7 +35,6 @@ class MainActivity : AppCompatActivity() {
         BottomNavigationView.OnNavigationItemSelectedListener { item ->
             var selectFragment: Fragment? = null
             when (item.itemId) {
-                R.id.Home -> selectFragment = HomeFragment()
                 R.id.Search -> selectFragment = SearchFragment()
                 R.id.Settings -> selectFragment = SettingsFragment()
             }
